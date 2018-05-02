8431f04edd871428ee9ad902bb1e7d0faf5467e1
bukharim96 <bukharim96@gmail.com>
2018-05-02 19:44:06 +0200

    Added support for xRepeat (first directive)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 // DirectiveX Parser
 
+module.exports = function (babel) {
+	const { types: t } = babel
+	
+	return {
+		inherits: require('babel-plugin-syntax-jsx'),
+		visitor: {
+			JSXElement(path) {
+				var attrs = path.node.openingElement.attributes.map(
+						attr => attr.name.name
+					),
+					replacement
+				
+				if (!attrs.length)
+					return
+
+				if (attrs.includes('xRepeat'))
+					path.replaceWith(
+						require('./xRepeat')(t, path, attrs)
+					)
+			}
+		}
+	}
+}
---
+++ src/xRepeat.js
@@ -0,0 +1,54 @@
+// xRepeat Parser
+
+module.exports = function (t, path, attrs) {
+	var openingEl = path.node.openingElement,
+		compIdentifier = t.JSXIdentifier(openingEl.name.name),
+		directive = openingEl.attributes[attrs.findIndex(
+			attr => attr === 'xRepeat')
+		],
+		{
+			value: {
+				expression: {
+					left: { name: toRepAsIdentifier },
+					right: toRepIdentifier
+				}
+			}
+		} = directive,
+		toRepIdentifierComputed = t.isStringLiteral(
+			toRepIdentifier.property
+		),
+		toRepIdentifier = toRepIdentifier.name
+						? t.identifier(toRepIdentifier.name)
+						: t.memberExpression(
+							toRepIdentifier.object,
+							toRepIdentifier.property,
+							toRepIdentifierComputed
+						),
+		children = path.node.children,
+		props = openingEl.attributes.filter(
+			prop => prop.name.name !== 'xRepeat'
+		)
+	
+	var returnStatement = t.JSXElement(
+			t.JSXOpeningElement(
+			  compIdentifier,
+			  props
+			),
+			t.JSXClosingElement(compIdentifier),
+			children,
+			false
+		)
+	
+	return t.callExpression(
+		t.memberExpression(toRepIdentifier, t.identifier('map')), [
+		t.arrowFunctionExpression(
+			[
+				t.identifier(toRepAsIdentifier),
+				t.identifier('i')
+			],
+			t.blockStatement([
+				t.returnStatement(returnStatement)
+			])
+		)
+	])
+}
