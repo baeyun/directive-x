1d6ffdd56bed3e2400f1ff2292998ca611509d0f
bukharim96 <bukharim96@gmail.com>
2018-05-10 01:36:32 +0200

    Tests case for xBind

--- tests/xBind.js
+++ tests/xBind.js
@@ -1,21 +1,19 @@
 class Button extends Component {
-	constructor(props) {
-		super(props)
-	}
-	
-	handleClick(promptMsg='Hello, World!') {
-		alert(promptMsg)
-	}
-	
+    constructor(props, stuff) {
+      super(props)
+    }
+  
+  	handleClick(e, promptMsg='Hello, World!') {
+      e.preventDefault()
+      alert(promptMsg)
+    }
+  
 	render() {
-		let { props: {promptMsg, tooltipMsg} } = this
-		
-		return(
-			<View
-				xBind={lol} onClick={this.handleClick(promptMsg)}
-				onHover={this.displayTooltip(tooltipMsg)}>
-				<Text children="Click me" />
-			</View>
-		)
-	}
+      let { props: {promptMsg, tooltipMsg} } = this
+      
+      return(
+        <View xBind onSubmit={this.submit(blablabla)} onClick={(e) => this.handleClick(promptMsg)}>
+            <Text xBind children="Click me" />
+        </View>)
+    }
 }
