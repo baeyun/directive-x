8a21d7e450bdac9e76a428cf40e5394c098136f2
bukharim96 <bukharim96@gmail.com>
2018-05-10 01:36:00 +0200

    1st functional commit of xBind

--- src/xBind.js
+++ src/xBind.js
@@ -1,85 +1,107 @@
 // xBind Parser
 
 module.exports = function (t, path, attrs) {
-	var openingEl = path.node.openingElement,
-		eventProps = openingEl.attributes.filter(
-			prop => prop.name.name.slice(0, 2) === 'on'
-		),
-		directive = openingEl.attributes[
-			attrs.findIndex(attr => attr === 'xBind')
-		],
-		bindTo = directive.value.expression.type !== 'ThisExpression'
-			   ? t.identifier(directive.value.expression.name)
-			   : t.thisExpression(),
-		classDeclaration = path.find((path) => path.isClassDeclaration()),
-		{
-			node: {
-				body: { body: classMethods }
-			}
-		} = classDeclaration,
-		constructorMethodKey = classMethods.map(
-			(cm, i) => cm.kind === 'constructor' && i
-		).filter(i => i !== false)[0],
-		oldConstructor = classDeclaration.node.body.body[constructorMethodKey],
-		xBindsClassMethodKey = classMethods.map(
-			(cm, i) => cm.key.name === 'xBinds' && i
-		).filter(i => i !== false)[0],
-		xBindsClassMethod = classDeclaration.node.body.body[xBindsClassMethodKey],
-		xBindsExists = oldConstructor.body.body.map(
-			v => v.expression.callee.property && v.expression.callee.property.name === 'xBinds'
-		).filter(v => v)[0],
-		bindToId = t.identifier('bindTo'),
-		eventCallbackId = t.identifier('handleClick')
-		
-		if (!xBindsExists) {
-			// construct new constructor
-			var newConstructor = t.classMethod(
-				'constructor',
-				t.identifier('constructor'),
-				oldConstructor.params,
-				oldConstructor.body
-			)
-			
-			// add xBinds call to constructor
-			newConstructor.body.body[oldConstructor.body.body.length] = t.expressionStatement(t.callExpression(
-				t.memberExpression(t.thisExpression(), t.identifier('xBinds')),
-				[bindTo]
-			))
+	let openingEl = path.node.openingElement
+        
+        if (!attrs.length || !attrs.includes('xBind'))
+          return
+        
+        var classDeclaration = path.find((path) => path.isClassDeclaration())
+        
+        // bail if component not a class
+        if (!classDeclaration)
+          throw path.buildCodeFrameError(
+            'xBind only works with Class based components'
+          )
+        
+        var compIdentifier = t.JSXIdentifier(openingEl.name.name),
+            eventProps = openingEl.attributes.filter(
+              prop => prop.name.name.slice(0, 2) === 'on'
+            ),
+            directive = openingEl.attributes[
+	    		attrs.findIndex(attr => attr === 'xBind')
+	    	],
+            bindTo = directive.value && directive.value.expression.type !== 'ThisExpression'
+               ? t.identifier(directive.value.expression.name)
+               : t.thisExpression(),
+            {
+                node: {
+                    body: { body: classMethods }
+                }
+            } = classDeclaration,
+            constructorMethodKey = classMethods.map((cm, i) => cm.kind === 'constructor' && i).filter(i => i !== false)[0],
+            constructor = (constructorMethodKey >= 0)
+                ? classDeclaration.node.body.body[constructorMethodKey]
+                : t.classMethod(
+                          'constructor',
+                          t.identifier('constructor'),
+                          [],
+                          t.blockStatement([])
+                        ),
+            xBindEvenHandlersClassMethodKey = classMethods.map((cm, i) => cm.key.name === 'xBindEvenHandlers' && i).filter(i => i !== false)[0],
+            xBindEvenHandlersClassMethod = classDeclaration.node.body.body[xBindEvenHandlersClassMethodKey],
+            xBindsCalledInConstructor = constructor.body.body.map(
+              v => v.expression && v.expression.callee.property && v.expression.callee.property.name === 'xBindEvenHandlers'
+            ).filter(v => v)[0],
+            eventCallbackId = t.identifier('handleClick'),
+            JSXOpeningElementSrc = path
+                            .getSource().split(/\>\n/)[0] // get opening JSX element
+                            .match(/(?:on\w+\s*\=\s*\{)*this\.(\w+)/gi),
+            eventHandlerIDs = JSXOpeningElementSrc && JSXOpeningElementSrc.map(str => {
+                              return t.identifier(str.replace(/on\w+\s*\=\s*\{/, '').replace(/this\./, ''))
+                            })
 
-			// Out with the old, in with the new
-			delete classDeclaration.node.body.body[constructorMethodKey]
-			classDeclaration.get('body').unshiftContainer('body', newConstructor)
-		}
-		
-		// prepend 'xBinds' method in class if !exist
-		if (!xBindsClassMethod) {
-			var xBindsClassMethod = t.classMethod(
-				'method',
-				t.identifier('xBinds'),
-				[bindToId],
-				t.blockStatement([])
-			)
-			
-			classDeclaration.get('body').unshiftContainer('body', xBindsClassMethod)
-		}
-		
-		// safely extend xBinds method
-		xBindsClassMethod.params = [bindToId]
-		xBindsClassMethod.body.body[xBindsClassMethod.body.body.length] = t.expressionStatement(
-			t.assignmentExpression(
-				'=',
-				t.memberExpression(t.thisExpression(), eventCallbackId),
-				t.callExpression(
-					t.memberExpression(
-						t.memberExpression(t.thisExpression(), eventCallbackId),
-						t.identifier('bind')
-					),
-					[bindToId]
-				)
-			)
-		)
-		
-		// get rid of xBind prop &&/|| update events
+        // get rid of xBind directive to prevent
+        // further transpilation
+        delete directive
+        
+        // Bail if there aren't any events in the first place
+        if (!eventHandlerIDs)
+        	throw path.buildCodeFrameError( // @todo warn instead of error
+	    		'Using xBind on event-less components is bad for performance'
+	        )
+        
+        if (!xBindsCalledInConstructor) {
+          // add xBinds call to constructor
+          constructor.body.body[constructor.body.body.length] = t.expressionStatement(
+            t.callExpression(
+              t.memberExpression(t.thisExpression(), t.identifier('xBindEvenHandlers')), []
+            )
+          )
+
+          // Out with the old, in with the new
+          delete classDeclaration.node.body.body[constructorMethodKey]
+          classDeclaration.get('body').unshiftContainer('body', constructor)
+        }
+        
+        // prepend 'xBindEvenHandlers' method in class if !exist
+        if (!xBindEvenHandlersClassMethod) {
+          var xBindEvenHandlersClassMethod = t.classMethod(
+            'method',
+            t.identifier('xBindEvenHandlers'),
+            [],
+            t.blockStatement([])
+          )
+          
+          classDeclaration.get('body').unshiftContainer('body', xBindEvenHandlersClassMethod)
+        }
+        
+        // safely extend xBindEvenHandlers class method
+        for (let i = 0; i < eventHandlerIDs.length; i++) {
+          xBindEvenHandlersClassMethod.body.body[xBindEvenHandlersClassMethod.body.body.length + i] = t.expressionStatement(
+            t.assignmentExpression(
+              '=',
+              t.memberExpression(t.thisExpression(), eventHandlerIDs[i]),
+              t.callExpression(
+                t.memberExpression(
+                  t.memberExpression(t.thisExpression(), eventHandlerIDs[i]),
+                  t.identifier('bind')
+                ),
+                [bindTo]
+              )
+            )
+          )
+        }
 
 		return path // doing nothing...just for now
 }
