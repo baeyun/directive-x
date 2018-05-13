8ad747327fd4748fac94f34cffd20473607b6d5e
bukharim96 <bukharim96@gmail.com>
2018-05-13 17:35:03 +0200

    Refactor and prettify xGet.js

--- src/xGet.js
+++ src/xGet.js
@@ -1,49 +1,49 @@
 // xGet Parser
 
 module.exports = function (t, path, attrs) {
-	var openingEl = path.node.openingElement,
-		compIdentifier = t.JSXIdentifier(openingEl.name.name),
-		props = openingEl.attributes.filter(
-			prop => prop.name.name !== 'xGet'
-		),
-		children = path.node.children,
-		directive = openingEl.attributes[attrs.findIndex(
-			attr => attr === 'xGet')
-		],
-		{
-			value: {
-				expression: {
-					left: varDeclaratorId,
-					right: varDeclaratorInit
-				}
-			}
-		} = directive,
-		varDeclaratorId = t.isIdentifier(varDeclaratorId)
-						? varDeclaratorId
-						: t.ObjectPattern(varDeclaratorId.properties),
-		requireVarsExp = t.VariableDeclaration('var', [
-			t.variableDeclarator(
-				varDeclaratorId,
-				t.callExpression(
-					t.identifier('require'),
-					[varDeclaratorInit]
-				)
-			)
-		]),
-		JSXElement = t.JSXElement(
-			t.JSXOpeningElement(
-				compIdentifier,
-				props
-			),
-			t.JSXClosingElement(compIdentifier),
-			children,
-			false
-		)
+  var openingEl = path.node.openingElement,
+    compIdentifier = t.JSXIdentifier(openingEl.name.name),
+    props = openingEl.attributes.filter(
+      prop => prop.name.name !== 'xGet'
+    ),
+    children = path.node.children,
+    directive = openingEl.attributes[attrs.findIndex(
+      attr => attr === 'xGet')
+    ],
+    {
+      value: {
+        expression: {
+          left: varDeclaratorId,
+          right: varDeclaratorInit
+        }
+      }
+    } = directive,
+    varDeclaratorId = t.isIdentifier(varDeclaratorId)
+      ? varDeclaratorId
+      : t.ObjectPattern(varDeclaratorId.properties),
+    requireVarsExp = t.VariableDeclaration('var', [
+      t.variableDeclarator(
+        varDeclaratorId,
+        t.callExpression(
+          t.identifier('require'),
+          [varDeclaratorInit]
+        )
+      )
+    ]),
+    JSXElement = t.JSXElement(
+      t.JSXOpeningElement(
+        compIdentifier,
+        props
+      ),
+      t.JSXClosingElement(compIdentifier),
+      children,
+      false
+    )
 
-	return t.arrowFunctionExpression([],
-		t.blockStatement([
-			requireVarsExp,
-			t.returnStatement(JSXElement)
-		])
-	)
+  return t.arrowFunctionExpression([],
+    t.blockStatement([
+      requireVarsExp,
+      t.returnStatement(JSXElement)
+    ])
+  )
 }
