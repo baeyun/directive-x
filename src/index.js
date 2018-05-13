93a988093db04ed34ab71b4b7072bd756045f86b
bukharim96 <bukharim96@gmail.com>
2018-05-13 17:33:51 +0200

    Refactor and prettify core (index.js)

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 // DirectiveX Parser
 
 module.exports = function (babel) {
-	const { types: t } = babel
-	
-	return {
-		inherits: require('babel-plugin-syntax-jsx'),
-		visitor: {
-			JSXElement(path) {
-				var attrs = path.node.openingElement.attributes.map(
-					attr => attr.name.name
-				)
-				
-				if (!attrs.length)
-					return
+  const { types: t } = babel
+  
+  return {
+    inherits: require('babel-plugin-syntax-jsx'),
+    visitor: {
+      JSXElement(path) {
+        var attrs = path.node.openingElement.attributes.map(
+          attr => attr.name.name
+        )
+        
+        if (!attrs.length)
+          return
 
-				if (attrs.includes('xGet'))
-					path.replaceWith(
-						require('./xGet')(t, path, attrs)
-					)
-				else if (attrs.includes('xBind'))
-					path.replaceWith(
-						require('./xBind')(t, path, attrs)
-					)
-				else if (attrs.includes('xRepeat'))
-					path.replaceWith(
-						require('./xRepeat')(t, path, attrs)
-					)
-				else if (attrs.includes('xIf'))
-					path.replaceWith(
-						require('./xIf')(t, path, attrs)
-					)
-			}
-		}
-	}
+        if (attrs.includes('xGet'))
+          path.replaceWith(
+            require('./xGet')(t, path, attrs)
+          )
+        else if (attrs.includes('xBind'))
+          path.replaceWith(
+            require('./xBind')(t, path, attrs)
+          )
+        else if (attrs.includes('xRepeat'))
+          path.replaceWith(
+            require('./xRepeat')(t, path, attrs)
+          )
+        else if (attrs.includes('xIf'))
+          path.replaceWith(
+            require('./xIf')(t, path, attrs)
+          )
+      }
+    }
+  }
 }
